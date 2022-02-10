635b3410e09838f361db251bcac19f49f82d34d0
techpluscraft <techpluscraft@gmail.com>
2022-02-10 15:07:15 -0500

    about

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+      Угадай слово с 6 попыток. После каждого предположения цвет плитки будет
+         измените, чтобы показать, насколько ваша догадка была близка к слову.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
