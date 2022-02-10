e5ac0253049b48c87cbec7ddccc4ea416d5baa15
techpluscraft <techpluscraft@gmail.com>
2022-02-10 15:56:44 -0500

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,43 +8,43 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Как играть" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
       Угадай слово с 6 попыток. После каждого предположения цвет плитки будет
          измените, чтобы показать, насколько ваша догадка была близка к слову.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="А" status="correct" />
+        <Cell value="Б" />
+        <Cell value="Б" />
+        <Cell value="А" status="correct"/>
+        <Cell value="Т" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        Буква А в слове и на правильном месте.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="А" />
+        <Cell value="Б" />
+        <Cell value="Б" />
+        <Cell value="А" />
+        <Cell value="Т" status="present"/>
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        Буква Т в слове есть, но не на том месте.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="А" />
+        <Cell value="Б" status="absent"/>
+        <Cell value="Б" status="absent"/>
+        <Cell value="А" />
+        <Cell value="Т" status="present"/>
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        Буква Б нет в слове ни в одном месте.
       </p>
     </BaseModal>
   )
